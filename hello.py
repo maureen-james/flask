155ebada8d4422c5ff36b3d750dc0a5a6139a31d
maureen-james <manjax97@gmail.com>
2022-04-28 01:36:08 +0300

    save to json

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
+import json
+# from requests import request
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', name='maureen')
+    return render_template('home.html')
 
 
-@app.route('/about')
-def about():
-    return 'Hello flask!'
+@app.route('/your-url')
+def your_url():
+    urls ={}
+    urls[request.args['code']]={'url':request.args['url']}
+    with open('urls.json','w') as url_file:
+        json.dump(urls,url_file) 
+    return render_template('your_url.html',code=request.args['code'])
